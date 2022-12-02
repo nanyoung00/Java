5dfc30c207bd382d1d6229088448a9d8841aa9de
nanyoung00 <nyungnim00@gmail.com>
2022-12-02 16:40:21 +0900

    first commit

---
+++ javajungsuk/src/javajungsuk/Hello.java
@@ -0,0 +1,11 @@
+package javajungsuk;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
---
+++ javajungsuk/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author wwiwtb
+ *
+ */
+module javajungsuk {
+}
