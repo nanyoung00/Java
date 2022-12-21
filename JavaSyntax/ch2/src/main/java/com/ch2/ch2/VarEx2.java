c2cbb44dac01d1316c8e7611ac2876cbf77b1e66
nanyoung00 <nyungnim00@gmail.com>
2022-12-21 23:24:04 +0900

    ch2-2finished

---
+++ JavaSyntax/ch2/src/main/java/com/ch2/ch2/VarEx2.java
@@ -0,0 +1,16 @@
+package com.ch2.ch2;
+
+class VarEx2 {
+    public static void main(String[] args) {
+        int x=10, y=20;
+        int tmp=0;
+
+        System.out.println("x:"+x+"y:"+y);
+
+        tmp=x;
+        x=y;
+        y=tmp;
+
+        System.out.println("x:"+x+"y:"+y);
+    }
+}
